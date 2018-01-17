eec8af071207e5caaa835c1eb54ffd6453eede32
= <=>
2018-01-17 13:07:40 +0900

    1월 16일 last question not finished

--- Winter_Vacation/Q.cpp
+++ Winter_Vacation/Q.cpp
@@ -1,56 +1,23 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-  //  I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
-
-
 int main()
 {
-    string str;
-    int iNum_Prev = 1001, iSum = 0, iNum_Current;
-    cin >> str;
-    for(int i=0; i<str.length(); ++i)
+    cin.sync_with_stdio(false); cin.tie(NULL);
+    int N;
+    cin >> N;
+    int iArray[N];
+    for(int i=0; i<N; ++i)
     {
-        switch(str[i])
-        {
-            case 'I':
-                iNum_Current = 1;
-                break;
-            case 'V':
-                iNum_Current = 5;
-                break;
-            case 'X':
-                iNum_Current = 10;
-                break;
-            case 'L':
-                iNum_Current = 50;
-                break;
-            case 'C':
-                iNum_Current = 100;
-                break;
-            case 'D':
-                iNum_Current = 500;
-                break;
-            case 'M':
-                iNum_Current = 1000;
-                break;
-        }
-        if(iSum == 0)
-            iSum+=iNum_Current;
-        else if(iNum_Prev >= iNum_Current)
-            iSum+=iNum_Current;
-        else {
-            iSum-=iNum_Prev;
-            iSum+=(iNum_Current - iNum_Prev);
-        }
-
-        iNum_Prev = iNum_Current;
+        cin >> iArray[i];
     }
 
-    cout << iSum << endl;
-    return 0;
+    sort(iArray, iArray+N);
+
+    for(int i=0; i<N; ++i)
+        cout << iArray[i] << '\n';
 }
 
 //1<<1, (1<<1) + 1
